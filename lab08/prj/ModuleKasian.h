d08f538c42e3ac96380e5c8fd8f66c412f31ee2b
Kasian Yurii <kasianyurol@kntu.kr.ua>
2026-04-03 18:11:25 +0300

    Доповнення заголовкового файлу

--- lab08/prj/ModuleKasian.h
+++ lab08/prj/ModuleKasian.h
@@ -1,8 +1,19 @@
-#ifndef MODULEKASIAN_H_INCLUDED
-#define MODULEKASIAN_H_INCLUDED
+#ifndef MODULESKASIAN_H_INCLUDED
+#define MODULESKASIAN_H_INCLUDED
 
+#include <string> // Необхідно для std::string
+
+// Прототипи функцій з ЛР 08 та 09
 double s_calculation(double x, double y, double z);
 int task9_1();
 int task9_2();
 int task9_3();
-#endif // MODULEKASIAN_H_INCLUDED
+
+// Прототипи функцій для ЛР 10
+// Використовуємо std:: перед string, щоб уникнути помилок компіляції
+// у файлах, де не прописано 'using namespace std'
+void Task10_1(std::string inFileName, std::string outFileName);
+void Task10_2(std::string fileName);
+void Task10_3(std::string fileName, double x, double y, double z, int b);
+
+#endif // MODULESKASIAN_H_INCLUDED
