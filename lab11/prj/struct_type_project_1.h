3303b4838d0336f80ea7ddf106a13117ca7e7393
Kasian Yurii <kasianyurol@kntu.kr.ua>
2026-05-02 21:04:04 +0300

    Доповнення звіту та створення заголовкового файлу зі структурою

---
+++ lab11/prj/struct_type_project_1.h
@@ -0,0 +1,18 @@
+#ifndef STRUCT_TYPE_PROJECT_1_H_INCLUDED
+#define STRUCT_TYPE_PROJECT_1_H_INCLUDED
+
+using namespace std;
+
+struct Reestr
+{
+    string l_name;
+    string f_name;
+    string s_name;
+    string marka;
+    int year;
+    string data;
+    string nomer;
+    string prumitku;
+};
+
+#endif // STRUCT_TYPE_PROJECT_1_H_INCLUDED
