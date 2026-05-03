2fce53f4d96161ec612f925eee568f3f4d2b503b
Kasian Yurii <kasianyurol@kntu.kr.ua>
2026-05-03 16:46:53 +0300

    перепис модуля і заголовкових файлів

--- lab11/prj/ModuleKasian/ModuleKasian.h
+++ lab11/prj/ModuleKasian/ModuleKasian.h
@@ -1,8 +1,9 @@
 #ifndef MODULEKASIAN_H_INCLUDED
 #define MODULEKASIAN_H_INCLUDED
 
-void Dodavannya(Reestr*& db, int& count, int& capacity);
-void searchnom(Reestr* db, int count);
-void loadDatabaseFromFile(Reestr*& db, int& count, int& capacity, string filename = "Reestr.txt");
-void saveDatabaseToFile(Reestr* db, int count, string filename = "Reestr.txt");
+#include "struct_type_project_1.h"
+#include <string>
+
+void Dodavannya(Reestr*& head);
+void searchnom(Reestr* head);
 #endif // MODULEKASIAN_H_INCLUDED
